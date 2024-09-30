d1295a3d33c160d4df1ee159d895d603a4d27d07
Jerry <jerryshum2636@gmail.com>
2024-09-30 17:28:30 -0400

    made nav absolute

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
    return (
-      <nav className="flex justify-between items-center px-10 py-10">
+      <nav className="flex justify-between items-center px-10 py-10 absolute w-full">
          <Link to={"/"}>
             <img
                src="../../public/Logo 3.png"
