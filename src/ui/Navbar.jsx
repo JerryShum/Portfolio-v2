d64b45b1b8ef6e38ff6430fc5919573b9520b0f4
Jerry <jerryshum2636@gmail.com>
2024-09-30 16:38:03 -0400

    Created the navbar

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,7 +1,30 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
    return (
-      <nav className="flex justify-between">
-         <img src="../../public/Logo 3.png" alt="logo" />
+      <nav className="flex justify-between items-center px-10 py-10">
+         <Link to={"/"}>
+            <img
+               src="../../public/Logo 3.png"
+               alt="logo"
+               className="h-[60px]"
+            />
+         </Link>
+
+         <ul className="flex gap-3 font-serif font-bold text-xl">
+            <li>
+               <Link>Works</Link>
+               <span>,</span>
+            </li>
+            <li>
+               <Link>About</Link>
+               <span>,</span>
+            </li>
+            <li>
+               <Link>Contact</Link>
+               <span>,</span>
+            </li>
+         </ul>
       </nav>
    );
 }
