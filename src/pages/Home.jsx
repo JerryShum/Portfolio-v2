d66f90365d7f5e5e9ce97efc40c06271ec66bc9b
Jerry <jerryshum2636@gmail.com>
2024-09-30 17:28:12 -0400

    adding noise overlay

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import HeroPage from "../sections/hero/HeroPage";
 import Navbar from "../ui/Navbar";
+import Overlay from "../ui/Overlay";
 
 function Home() {
    return (
       <>
          <Navbar />
-         <HeroPage></HeroPage>
+         <HeroPage />
+         <Overlay />
       </>
    );
 }
