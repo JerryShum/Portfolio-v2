c5714750fa1a8a0468fc85ff3c768bc51865f13c
Jerry <jerryshum2636@gmail.com>
2024-10-02 13:58:31 -0400

    general structure of the works page

--- src/sections/works/Works.jsx
+++ src/sections/works/Works.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import ProjectCard from "../../ui/ProjectCard";
+
 function Works() {
+  const [projectCount, setProjectCount] = useState(1);
+
   return (
-    <section>
-      <h2 className="font-serif text-8xl font-bold">Selected Works</h2>
+    <section className="h-screen p-10">
+      <h2 className="font-serif text-8xl font-bold tracking-wider">
+        Selected Works
+      </h2>
+      <div className="flex">
+        <h3 className="basis-5/12 text-[300px] tracking-tighter">
+          0{projectCount}.
+        </h3>
+        <div className="grow">
+          <ProjectCard />
+        </div>
+      </div>
     </section>
   );
 }
