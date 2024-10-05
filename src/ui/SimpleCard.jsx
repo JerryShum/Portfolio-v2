108fc41eda7edc01cfd29f5d70461e13972041fe
Jerry <jerryshum2636@gmail.com>
2024-10-05 15:21:56 -0400

    creating simple card component for about section

---
+++ src/ui/SimpleCard.jsx
@@ -0,0 +1,9 @@
+function SimpleCard() {
+  return (
+    <div className="rounded-lg bg-slate-500 p-10 odd:mb-20 even:mt-20">
+      <h3>Skil Goes Here</h3>
+    </div>
+  );
+}
+
+export default SimpleCard;
