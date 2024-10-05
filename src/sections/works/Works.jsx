f893ce84aaac20062c0fce3d1ce9208f1b849321
Jerry <jerryshum2636@gmail.com>
2024-10-05 15:22:09 -0400

    rounding off projects container

--- src/sections/works/Works.jsx
+++ src/sections/works/Works.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import ProjectCard from "../../ui/ProjectCard";
 import projectsData from "../../projects.json";
-import styles from "./Works.module.css";
 
 function Works() {
   const [projectCount, setProjectCount] = useState(1);
@@ -15,7 +14,7 @@ function Works() {
         <h3 className="line basis-5/12 text-[300px] leading-tight tracking-tighter">
           0{projectCount}.
         </h3>
-        <div className="no-scrollbar-x h-screen grow space-y-24 overflow-scroll">
+        <div className="no-scrollbar-x h-screen grow space-y-24 overflow-scroll rounded-lg">
           {projectsData.map((project, index) => (
             <ProjectCard {...project} key={index} />
           ))}
