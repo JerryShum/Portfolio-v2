ad1ce41500b1ff46e08ba056485390353b5e0d52
Jerry <jerryshum2636@gmail.com>
2024-10-05 14:12:50 -0400

    Adding about page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,18 @@ import HeroPage from "../sections/hero/HeroPage";
 import Navbar from "../ui/Navbar";
 import Overlay from "../ui/Overlay";
 import Works from "../sections/works/Works";
+import About from "../sections/about/About";
 
 function Home() {
-   return (
-      <>
-         <Overlay />
-         <Navbar />
-         <HeroPage />
-         <Works />
-      </>
-   );
+  return (
+    <>
+      <Overlay />
+      <Navbar />
+      <HeroPage />
+      <Works />
+      <About />
+    </>
+  );
 }
 
 export default Home;
--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,5 +1,12 @@
 function About() {
-   return <div>about</div>;
+  return (
+    <section className="p-10">
+      <h2 className="mb-20 font-serif text-8xl font-bold tracking-wider">
+        Selected Works
+      </h2>
+      <div className="flex"></div>
+    </section>
+  );
 }
 
 export default About;
