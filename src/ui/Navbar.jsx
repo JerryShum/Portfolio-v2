f1dfa6c5f1f5b923010f7cae832ce1133c304658
Jerry <jerryshum2636@gmail.com>
2024-10-06 23:55:19 -0400

    stylibng

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,31 +1,27 @@
 import { Link } from "react-router-dom";
 
 function Navbar() {
-   return (
-      <nav className="flex justify-between items-center px-10 py-10 absolute w-full">
-         <Link to={"/"}>
-            <img
-               src="../../public/Logo 3.png"
-               alt="logo"
-               className="h-[60px]"
-            />
-         </Link>
+  return (
+    <nav className="absolute flex w-full items-center justify-between px-10 py-10">
+      <Link to={"/"}>
+        <img src="../../public/Logo 3.png" alt="logo" className="h-[60px]" />
+      </Link>
 
-         <ul className="flex gap-3 font-serif font-bold text-xl">
-            <li>
-               <Link>Works</Link>
-            </li>
-            <li>|</li>
-            <li>
-               <Link>About</Link>
-            </li>
-            <li>|</li>
-            <li>
-               <Link>Contact</Link>
-            </li>
-         </ul>
-      </nav>
-   );
+      <ul className="flex gap-3 font-serif text-xl font-bold">
+        <li>
+          <Link>Works</Link>
+        </li>
+        <li>|</li>
+        <li>
+          <Link>About</Link>
+        </li>
+        <li>|</li>
+        <li>
+          <Link>Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
