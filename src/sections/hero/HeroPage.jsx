a68b15af2ad02ce4130d6fd09fa7911b4fbc4a72
Jerry <jerryshum2636@gmail.com>
2024-10-07 20:57:46 -0400

    fixing some styling

--- src/sections/hero/HeroPage.jsx
+++ src/sections/hero/HeroPage.jsx
@@ -3,7 +3,7 @@ import styles from "./HeroPage.module.css";
 function HeroPage() {
   return (
     <section
-      className={`${styles.sectionbg} flex h-screen flex-col items-center justify-center gap-5`}
+      className={`${styles.sectionbg} relative flex h-screen flex-col items-center justify-center gap-5`}
     >
       <h1 className="flex items-center justify-center font-serif text-9xl font-bold tracking-wider">
         Jerry Shum
@@ -11,6 +11,7 @@ function HeroPage() {
       <h2 className="font-serif text-4xl font-semibold tracking-wider">
         welcome to the show
       </h2>
+
       <img
         className="absolute bottom-24"
         src="/icons/🦆 icon _chevron bottom_.svg"
