b4a3b9f72347c75362f31c4f4af071d54d0f76b4
Jerry <jerryshum2636@gmail.com>
2024-10-10 16:55:11 -0400

    changed router to include applayout

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,25 @@ import { createRoot } from "react-dom/client";
 import Home from "./pages/Home.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./ui/AppLayout.jsx";
+import Error404 from "./pages/Error404.jsx";
 
 const router = createBrowserRouter([
-   {
-      path: "/",
-      element: <Home />,
-   },
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error404 />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
-   <StrictMode>
-      <RouterProvider router={router} />
-   </StrictMode>
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
 );
