9df0357b7d731d1e5913a55ab7792598e5fdd898
Jerry <jerryshum2636@gmail.com>
2024-10-14 00:50:04 -0400

    defining project grid

---
+++ src/sections/projectsgrid/ProjectsGrid.jsx
@@ -0,0 +1,14 @@
+import ProjectsHoverCard from "./ProjectsHoverCard";
+import projectsData from "../../projects.json";
+
+function ProjectsGrid() {
+  return (
+    <div className="mt-10 grid grid-rows-2 gap-3 px-5 lg:grid-cols-2 lg:gap-5 lg:px-60">
+      {projectsData.map((project, index) => (
+        <ProjectsHoverCard project={project} key={index} />
+      ))}
+    </div>
+  );
+}
+
+export default ProjectsGrid;
