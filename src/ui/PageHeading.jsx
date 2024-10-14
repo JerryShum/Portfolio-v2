d00ed51b5d264a514f42557a3564d665de81d3fb
Jerry <jerryshum2636@gmail.com>
2024-10-14 00:02:22 -0400

    created a responsive page heading component

---
+++ src/ui/PageHeading.jsx
@@ -0,0 +1,11 @@
+function PageHeading({ children }) {
+  return (
+    <div className="bg-gradient-to-b from-blue-900 from-10% to-black px-4 py-10 md:px-20 md:py-16 lg:px-40 lg:py-20">
+      <h1 className="text-center font-serif text-2xl font-semibold md:text-4xl xl:text-6xl">
+        {children}
+      </h1>
+    </div>
+  );
+}
+
+export default PageHeading;
