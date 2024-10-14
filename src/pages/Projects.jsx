5f62763f2a86823ef001b157561d902d1a213fc0
Jerry <jerryshum2636@gmail.com>
2024-10-14 00:50:27 -0400

    defining projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,14 @@
+import ProjectsGrid from "../sections/projectsgrid/ProjectsGrid";
+import PageHeading from "../ui/PageHeading";
+import projectsData from "../../projects.json";
+
 function Projects() {
-  return <></>;
+  return (
+    <>
+      <PageHeading>Recent Projects</PageHeading>
+      <ProjectsGrid />
+    </>
+  );
 }
 
 export default Projects;
