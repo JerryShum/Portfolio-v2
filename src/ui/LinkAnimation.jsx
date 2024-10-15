c24cffd212c7993c8f6768f8be9e435ad761b89c
Jerry <jerryshum2636@gmail.com>
2024-10-14 23:03:21 -0400

    Making a link animation

---
+++ src/ui/LinkAnimation.jsx
@@ -0,0 +1,70 @@
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+
+function LinkAnimation({ children, destination }) {
+  const DURATION = 0.25;
+  const STAGGER = 0.025;
+
+  return (
+    <Link to={destination}>
+      <motion.div
+        initial="initial"
+        whileHover="hovered"
+        className="relative block overflow-hidden whitespace-nowrap"
+      >
+        <div>
+          {children.split("").map((letter, index) => {
+            return (
+              <motion.span
+                className="inline-block"
+                key={index}
+                variants={{
+                  initial: {
+                    y: 0,
+                  },
+                  hovered: {
+                    y: "-100%",
+                  },
+                }}
+                transition={{
+                  duration: DURATION,
+                  ease: "easeInOut",
+                  delay: STAGGER * index,
+                }}
+              >
+                {letter}
+              </motion.span>
+            );
+          })}
+        </div>
+        <div className="absolute inset-0">
+          {children.split("").map((letter, index) => {
+            return (
+              <motion.span
+                className="inline-block"
+                key={index}
+                variants={{
+                  initial: {
+                    y: "100%",
+                  },
+                  hovered: {
+                    y: "0%",
+                  },
+                }}
+                transition={{
+                  duration: DURATION,
+                  ease: "easeInOut",
+                  delay: STAGGER * index,
+                }}
+              >
+                {letter}
+              </motion.span>
+            );
+          })}
+        </div>
+      </motion.div>
+    </Link>
+  );
+}
+
+export default LinkAnimation;
