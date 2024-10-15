9a9140d561ecef41a5eb3cfc818daa9fe938221b
Jerry <jerryshum2636@gmail.com>
2024-10-14 23:31:26 -0400

    adjusting animation values for project count

--- src/sections/works/Works.jsx
+++ src/sections/works/Works.jsx
@@ -7,7 +7,7 @@ function Works() {
   const [projectCount, setProjectCount] = useState(1);
 
   return (
-    <section className="mt-20 p-4 sm:p-10 xl:px-60">
+    <section className="mt-20 p-4 sm:p-10 xl:px-40 2xl:px-60">
       <h2 className="mb-10 text-center font-serif text-4xl font-bold tracking-wider sm:mb-20 sm:text-6xl">
         Selected Works
       </h2>
@@ -19,9 +19,9 @@ function Works() {
               <motion.span
                 className="absolute"
                 key={projectCount} // Use projectCount as a key to trigger the animation
-                initial={{ opacity: 0, y: -20 }} // Start slightly above and invisible
+                initial={{ opacity: 0, y: "-20%" }} // Start slightly above and invisible
                 animate={{ opacity: 1, y: 0 }} // Fade in and move to original position
-                exit={{ opacity: 0, y: 20 }} // Move down slightly and fade out
+                exit={{ opacity: 0, y: "20%" }} // Move down slightly and fade out
                 transition={{
                   duration: 0.75, // Set duration for the transitions
                   ease: [0.68, -0.55, 0.27, 1.55], // Apply a custom easing function for smoothness
