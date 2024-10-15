23e0fae2e94bfd56f868dfd2992e79b081dd801f
Jerry <jerryshum2636@gmail.com>
2024-10-15 14:32:29 -0400

    starting projecthovercard

--- src/sections/projectsgrid/ProjectsHoverCard.jsx
+++ src/sections/projectsgrid/ProjectsHoverCard.jsx
@@ -1,8 +1,18 @@
+import { motion } from "framer-motion";
+
 function ProjectsHoverCard({ project }) {
   return (
-    <div className="overflow-hidden rounded-lg lg:rounded-xl">
-      <img src={project.projectImage} />
-    </div>
+    <motion.div className="relative overflow-hidden rounded-lg lg:rounded-xl">
+      <img
+        src={project.projectImage}
+        className="transition-all duration-300 hover:brightness-75"
+      />
+      <div className="absolute top-[10%] w-full bg-black bg-opacity-70 py-2 text-center font-serif text-6xl tracking-tighter">
+        <motion.div>
+          <span>{project.projectName}</span>
+        </motion.div>
+      </div>
+    </motion.div>
   );
 }
 
