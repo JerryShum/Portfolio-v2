6f877e2da94729dc29acebf2dce97206ce862072
Jerry <jerryshum2636@gmail.com>
2024-10-15 20:02:40 -0400

    editing button

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 function Button({ children }) {
   return (
     <Link to={".."}>
-      <button className="mt-10 rounded-full bg-slate-300 p-4 px-5 font-serif text-lg font-semibold text-black md:text-2xl md:font-medium xl:font-bold">
+      <motion.button
+        className="mt-10 rounded-full bg-slate-300 p-4 px-5 font-serif text-lg font-semibold text-black transition-all duration-200 hover:bg-slate-400 md:text-2xl md:font-medium xl:font-bold"
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+      >
         {children}
-      </button>
+      </motion.button>
     </Link>
   );
 }
