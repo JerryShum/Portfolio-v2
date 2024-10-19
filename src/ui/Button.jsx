42350920ed25e626ba5aa9f84cb125462083291e
Jerry <jerryshum2636@gmail.com>
2024-10-19 02:36:40 -0400

    fixed margin issue

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -5,7 +5,7 @@ function Button({ children }) {
   return (
     <Link to={".."}>
       <motion.button
-        className="mt-10 rounded-full bg-slate-300 p-4 px-5 font-serif text-lg font-semibold text-black transition-all duration-200 hover:bg-slate-400 md:text-2xl md:font-medium xl:font-bold"
+        className="rounded-full bg-slate-300 p-3 px-4 font-serif text-base font-semibold text-black transition-all duration-200 hover:bg-slate-400 sm:p-4 sm:px-5 sm:text-lg md:text-xl md:font-medium lg:text-2xl xl:text-3xl xl:font-bold"
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
       >
