aebc4957be22806ce0bbc5ad0b56ea4b4ed94a04
Jerry <jerryshum2636@gmail.com>
2024-10-19 02:36:09 -0400

    defined project detail page

--- src/sections/projectsgrid/ProjectDetail.jsx
+++ src/sections/projectsgrid/ProjectDetail.jsx
@@ -1,22 +1,56 @@
 import { useParams } from "react-router-dom";
-
 import projectsData from "../../projects.json";
 import PageHeading from "../../ui/PageHeading";
+import { motion } from "framer-motion";
+import Button from "../../ui/Button";
+import FooterComponent from "../../ui/FooterComponent";
 
 function ProjectDetail() {
-  const { projectName } = useParams();
-  const project = projectsData.find(
-    (project) => project.projectName === projectName,
-  );
+  const { projectID } = useParams();
+  const project = projectsData.find((project) => project.id === projectID);
 
   if (!project) {
-    throw new Error("Project not found!");
+    throw new Error("Project not found!" + projectID);
   }
 
   return (
     <>
-      <PageHeading>{projectName}</PageHeading>
-      <div className="flex gap-10"></div>
+      <PageHeading>{project.projectName}</PageHeading>
+      <div className="my-20 grid items-center justify-center gap-4 px-6 md:px-10 lg:grid-cols-11 lg:px-20 xl:mb-40 xl:gap-10 2xl:px-72">
+        <motion.img
+          src={project.projectImage}
+          className="col-span-11 rounded-[30px] lg:col-span-6 lg:mb-0"
+        />
+        <div className="col-span-11 flex flex-col items-center gap-6 rounded-[30px] p-4 md:p-6 lg:col-span-5 xl:px-10 xl:py-6">
+          <p className="w-full border-b py-1 text-start text-sm font-semibold uppercase text-yellow-300 md:text-base lg:text-lg xl:text-xl">
+            Category:{" "}
+            <span className="block text-sm font-normal capitalize text-white md:text-base lg:text-lg">
+              {project.category}
+            </span>
+          </p>
+          <p className="w-full border-b py-1 text-start text-sm font-semibold uppercase text-green-300 md:text-base lg:text-lg xl:text-xl">
+            Year:{" "}
+            <span className="block text-sm font-normal capitalize text-white md:text-base lg:text-lg">
+              {project.year}
+            </span>
+          </p>
+          <p className="w-full border-b py-1 text-start text-sm font-semibold uppercase text-blue-300 md:text-base lg:text-lg xl:text-xl">
+            Skills:{" "}
+            <span className="block text-sm font-normal capitalize text-white md:text-base lg:text-lg">
+              {project.skills}
+            </span>
+          </p>
+          <p className="w-full border-b py-1 text-start text-sm font-semibold uppercase text-red-300 md:text-base lg:text-lg xl:text-xl">
+            Description:{" "}
+            <span className="block text-sm font-normal capitalize text-white md:text-base lg:text-lg">
+              {project.description}
+            </span>
+          </p>
+
+          <Button>View Project</Button>
+        </div>
+      </div>
+      <FooterComponent />
     </>
   );
 }
