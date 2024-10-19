ab23e656fa0c45314d5f7d1ace833bb966cbfd41
Jerry <jerryshum2636@gmail.com>
2024-10-19 02:36:58 -0400

    capitalize page heading

--- src/ui/PageHeading.jsx
+++ src/ui/PageHeading.jsx
@@ -1,7 +1,7 @@
 function PageHeading({ children }) {
   return (
     <div className="bg-gradient-to-b from-blue-900 from-10% to-black px-4 py-10 md:px-20 md:py-16 lg:px-40 lg:py-20">
-      <h1 className="text-center font-serif text-2xl font-semibold md:text-4xl xl:text-6xl">
+      <h1 className="text-center font-serif text-2xl font-semibold capitalize md:text-4xl xl:text-6xl">
         {children}
       </h1>
     </div>
