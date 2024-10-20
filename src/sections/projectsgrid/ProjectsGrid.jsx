f6434a851a06078ee396e9884de4192946727475
Jerry <jerryshum2636@gmail.com>
2024-10-19 21:19:35 -0400

    adjusting breakpoint padding

--- src/sections/projectsgrid/ProjectsGrid.jsx
+++ src/sections/projectsgrid/ProjectsGrid.jsx
@@ -3,7 +3,7 @@ import projectsData from "../../projects.json";
 
 function ProjectsGrid() {
   return (
-    <div className="my-10 grid grid-rows-2 gap-3 px-5 lg:grid-cols-2 lg:gap-5 lg:px-60">
+    <div className="my-10 grid grid-rows-2 gap-3 px-5 lg:grid-cols-2 lg:gap-5 lg:px-20 xl:px-60">
       {projectsData.map((project, index) => (
         <ProjectsHoverCard project={project} key={index} />
       ))}
