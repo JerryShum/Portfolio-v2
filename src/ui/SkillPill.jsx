b777bb08476b31821570622bb6c03902597650a0
Jerry <jerryshum2636@gmail.com>
2024-10-24 08:20:49 -0400

    doesn't span the entire width of the container now

--- src/ui/SkillPill.jsx
+++ src/ui/SkillPill.jsx
@@ -1,7 +1,7 @@
 function SkillPill({ skill, year }) {
   return (
     <div
-      className={`rounded-full border px-2 py-1 text-center text-base font-semibold uppercase lg:border-[1px] lg:py-2 ${
+      className={`inline-block rounded-full border px-2 py-1 text-center text-base font-semibold uppercase lg:border-[1px] lg:py-2 ${
         skill
           ? "border-slate-400 text-slate-400" // Style for skill
           : "border-b-neutral-300 text-neutral-400" // Style for year
