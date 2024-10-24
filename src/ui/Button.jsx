ac2a530ad0f80e8df3ac3ba26442a79558a8f106
Jerry <jerryshum2636@gmail.com>
2024-10-24 08:20:37 -0400

    fixed button, it routes back to the previous page

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 function Button({ children }) {
   return (
-    <Link to={".."}>
+    <Link to={-1}>
       <motion.button
         className="rounded-full bg-slate-300 p-3 px-4 font-serif text-base font-semibold text-black transition-all duration-200 hover:bg-slate-400 sm:p-4 sm:px-5 sm:text-lg md:text-xl md:font-medium lg:text-2xl xl:text-3xl xl:font-bold"
         whileHover={{ scale: 1.1 }}
