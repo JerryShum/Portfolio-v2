a96491155608ecb20513fc3874d578fc6f776301
Jerry <jerryshum2636@gmail.com>
2024-10-24 08:21:09 -0400

    added animation to the grid

--- src/sections/projectsgrid/ProjectsGrid.jsx
+++ src/sections/projectsgrid/ProjectsGrid.jsx
@@ -1,13 +1,26 @@
 import ProjectsHoverCard from "./ProjectsHoverCard";
 import projectsData from "../../projects.json";
+import { motion } from "framer-motion";
 
 function ProjectsGrid() {
   return (
-    <div className="my-10 grid grid-rows-2 gap-3 px-5 lg:grid-cols-2 lg:gap-5 lg:px-20 xl:px-60">
+    <motion.div
+      className="my-10 grid grid-rows-2 gap-3 px-5 lg:grid-cols-2 lg:gap-5 lg:px-20 xl:px-60"
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        opacity: 1,
+      }}
+      transition={{
+        ease: "easeInOut",
+        duration: 0.5,
+      }}
+    >
       {projectsData.map((project, index) => (
         <ProjectsHoverCard project={project} key={index} />
       ))}
-    </div>
+    </motion.div>
   );
 }
 
