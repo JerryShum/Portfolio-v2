eaf9ea5fdfaaf0a02778d38dbef6ac8efbd55a76
Jerry <jerryshum2636@gmail.com>
2024-10-24 08:21:00 -0400

    added animation to the page heading

--- src/ui/PageHeading.jsx
+++ src/ui/PageHeading.jsx
@@ -1,9 +1,25 @@
+import { motion } from "framer-motion";
+
 function PageHeading({ children }) {
   return (
     <div className="bg-gradient-to-b from-blue-900 from-10% to-black px-4 py-10 md:px-20 md:py-16 lg:px-40 lg:py-20">
-      <h1 className="text-center font-serif text-2xl font-semibold capitalize md:text-4xl xl:text-6xl">
+      <motion.h1
+        className="text-center font-serif text-2xl font-semibold capitalize md:text-4xl xl:text-6xl"
+        initial={{
+          y: -100,
+          opacity: 0,
+        }}
+        animate={{
+          y: 0,
+          opacity: 1,
+        }}
+        transition={{
+          ease: "easeInOut",
+          duration: 0.5,
+        }}
+      >
         {children}
-      </h1>
+      </motion.h1>
     </div>
   );
 }
