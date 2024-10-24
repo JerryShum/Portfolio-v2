322d2a3c1697b71d14bd59f3e85910cccf33657b
Jerry <jerryshum2636@gmail.com>
2024-10-24 08:41:15 -0400

    Remaking the about section

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -2,15 +2,11 @@ import SimpleCard from "../../ui/SimpleCard.jsx";
 
 function About() {
   return (
-    <section className="relative mt-40 p-10 md:px-20 lg:px-60">
+    <section className="relative mt-40 p-10 md:px-20 xl:px-60">
       <h2 className="z-10 mb-20 text-center font-serif text-4xl font-semibold tracking-tighter md:text-5xl lg:text-6xl">
         Who I am - in a nutshell
       </h2>
-      <div className="grid grid-cols-1 gap-10 md:grid-cols-2 md:gap-20 lg:grid-cols-3">
-        <SimpleCard />
-        <SimpleCard />
-        <SimpleCard />
-      </div>
+      <div></div>
     </section>
   );
 }
