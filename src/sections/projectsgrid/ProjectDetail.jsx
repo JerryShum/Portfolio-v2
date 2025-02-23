4963397f56176cc0d289526ac889f12dc5e3b717
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-02-23 14:57:28 -0500

    cleanup

--- src/sections/projectsgrid/ProjectDetail.jsx
+++ src/sections/projectsgrid/ProjectDetail.jsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 import Button from "../../ui/Button";
 import FooterComponent from "../../ui/Layout/FooterComponent";
 import SkillPill from "../../ui/SkillPill";
+import { useEffect } from "react";
 
 function ProjectDetail() {
   const { projectID } = useParams();
@@ -25,19 +26,19 @@ function ProjectDetail() {
         <div className="col-span-11 flex flex-col items-center gap-6 rounded-[30px] p-4 md:p-6 lg:col-span-5 xl:px-10 xl:py-6">
           <p className="w-full border-b py-1 text-start font-serif text-sm font-semibold uppercase text-yellow-300 md:text-base lg:text-lg xl:text-xl">
             Category:{" "}
-            <span className="font-sans block text-sm font-normal capitalize text-white md:text-base lg:text-lg">
+            <span className="block font-sans text-sm font-normal capitalize text-white md:text-base lg:text-lg">
               {project.category}
             </span>
           </p>
           <p className="w-full border-b py-1 text-start font-serif text-sm font-semibold uppercase text-green-300 md:text-base lg:text-lg xl:text-xl">
             Year:{" "}
-            <span className="font-sans block text-sm font-normal capitalize text-white md:text-base lg:text-lg">
+            <span className="block font-sans text-sm font-normal capitalize text-white md:text-base lg:text-lg">
               {project.year}
             </span>
           </p>
           <p className="w-full border-b py-1 text-start font-serif text-sm font-semibold uppercase text-blue-300 md:text-base lg:text-lg xl:text-xl">
             Skills:{" "}
-            <span className="font-sans block space-x-4 text-sm font-normal capitalize text-white md:text-base lg:text-lg">
+            <span className="block space-x-4 font-sans text-sm font-normal capitalize text-white md:text-base lg:text-lg">
               {/* {project.skills.map((skill) => (
                 <SkillPill skill={skill} />
               ))} */}
@@ -46,7 +47,7 @@ function ProjectDetail() {
           </p>
           <p className="w-full border-b py-1 text-start font-serif text-sm font-semibold uppercase text-red-300 md:text-base lg:text-lg xl:text-xl">
             Description:{" "}
-            <span className="font-sans block text-sm font-normal capitalize text-white md:text-base lg:text-lg">
+            <span className="block font-sans text-sm font-normal capitalize text-white md:text-base lg:text-lg">
               {project.description}
             </span>
           </p>
