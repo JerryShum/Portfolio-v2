31ffbb8a2e0ca81779197f713e7342b480a0ce87
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-02-23 14:57:11 -0500

    adjusting "what-i-do" information

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -4,24 +4,24 @@ function About() {
   const data = [
     {
       id: 1,
-      title: "Web Development",
+      title: "Frontend Development",
       description:
-        "Creating modern, responsive, and accessible web applications. Proficient in developing full-stack web applications using modern technologies and best practices.",
-      skills: ["React", "JavaScript", "CSS/HTML"],
+        "Crafting engaging user interfaces with modern frameworks and libraries. Focused on creating responsive, accessible, and performant web applications with smooth animations and interactions.",
+      skills: ["React", "TypeScript", "TailwindCSS", "Framer Motion"],
     },
     {
-      id: 2,
-      title: "Web Development",
+      id: 1,
+      title: "Frontend Development",
       description:
-        "Creating modern, responsive, and accessible web applications. Proficient in developing full-stack web applications using modern technologies and best practices.",
-      skills: ["React", "JavaScript", "CSS/HTML"],
+        "Crafting engaging user interfaces with modern frameworks and libraries. Focused on creating responsive, accessible, and performant web applications with smooth animations and interactions.",
+      skills: ["React", "TypeScript", "TailwindCSS", "Framer Motion"],
     },
     {
-      id: 3,
-      title: "Web Development",
+      id: 1,
+      title: "Frontend Development",
       description:
-        "Creating modern, responsive, and accessible web applications. Proficient in developing full-stack web applications using modern technologies and best practices.",
-      skills: ["React", "JavaScript", "CSS/HTML"],
+        "Crafting engaging user interfaces with modern frameworks and libraries. Focused on creating responsive, accessible, and performant web applications with smooth animations and interactions.",
+      skills: ["React", "TypeScript", "TailwindCSS", "Framer Motion"],
     },
   ];
   return (
@@ -30,105 +30,12 @@ function About() {
         What I do - in a nutshell
       </h2>
       <div className="mb-20 w-full">
-        <div className="mt-12 flex min-h-[150vh] flex-col justify-center gap-y-16">
+        <div className="mt-0 flex min-h-[150vh] flex-col justify-center gap-y-16">
           {data.map((skill, index) => (
             <AboutCard key={index} {...skill} index={index} />
           ))}
         </div>
       </div>
-
-      {/* <div className="pt-lg mb-40 w-full">
-        <div className="mt-12 flex flex-col justify-between gap-y-16">
-        
-          <div className="sticky top-[10%] rounded-lg border-t-2 border-slate-700 bg-black pt-4 sm:pt-8">
-            <div className="mb-12 lg:mb-20">
-              <div className="flex gap-4 text-3xl font-semibold md:text-5xl">
-                <span className="col-span-2">01.</span>
-                <h3 className="col-span-6 col-start-6">Web Development</h3>
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-4 md:grid md:grid-cols-12 md:gap-10">
-              <div className="col-span-7 col-start-6 space-y-4">
-                <p className="text-base font-light text-slate-300 md:max-w-[20rem]">
-                  Creating modern, responsive, and accessible web applications.
-                  Proficient in developing full-stack web applications using
-                  modern technologies and best practices.
-                </p>
-                <div className="flex flex-wrap gap-2 sm:gap-4">
-                  <div className="rounded-full bg-gray-200 px-2 py-1 text-black sm:px-4 sm:py-2">
-                    React
-                  </div>
-                  <div className="rounded-full bg-gray-200 px-2 py-1 text-black sm:px-4 sm:py-2">
-                    JavaScript
-                  </div>
-                  <div className="rounded-full bg-gray-200 px-2 py-1 text-black sm:px-4 sm:py-2">
-                    CSS/HTML
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="sticky top-[20%] rounded-lg border-t-2 border-slate-700 bg-black pt-4 sm:pt-8">
-            <div className="mb-12 lg:mb-20">
-              <div className="flex gap-4 text-3xl font-semibold md:text-5xl">
-                <span className="col-span-2">01.</span>
-                <h3 className="col-span-6 col-start-6">Web Development</h3>
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-4 md:grid md:grid-cols-12 md:gap-10">
-              <div className="col-span-7 col-start-6 space-y-4">
-                <p className="text-base font-light text-slate-300 md:max-w-[20rem]">
-                  Creating modern, responsive, and accessible web applications.
-                  Proficient in developing full-stack web applications using
-                  modern technologies and best practices.
-                </p>
-                <div className="flex flex-wrap gap-2 sm:gap-4">
-                  <div className="rounded-full bg-gray-200 px-2 py-1 text-black sm:px-4 sm:py-2">
-                    React
-                  </div>
-                  <div className="rounded-full bg-gray-200 px-2 py-1 text-black sm:px-4 sm:py-2">
-                    JavaScript
-                  </div>
-                  <div className="rounded-full bg-gray-200 px-2 py-1 text-black sm:px-4 sm:py-2">
-                    CSS/HTML
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="sticky top-[30%] rounded-lg border-t-2 border-slate-700 bg-black pt-4 sm:pt-8">
-            <div className="mb-12 lg:mb-20">
-              <div className="flex gap-4 text-3xl font-semibold md:text-5xl">
-                <span className="col-span-2">01.</span>
-                <h3 className="col-span-6 col-start-6">Web Development</h3>
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-4 md:grid md:grid-cols-12 md:gap-10">
-              <div className="col-span-7 col-start-6 space-y-4">
-                <p className="text-base font-light text-slate-300 md:max-w-[20rem]">
-                  Creating modern, responsive, and accessible web applications.
-                  Proficient in developing full-stack web applications using
-                  modern technologies and best practices.
-                </p>
-                <div className="flex flex-wrap gap-2 sm:gap-4">
-                  <div className="rounded-full bg-gray-200 px-2 py-1 text-black sm:px-4 sm:py-2">
-                    React
-                  </div>
-                  <div className="rounded-full bg-gray-200 px-2 py-1 text-black sm:px-4 sm:py-2">
-                    JavaScript
-                  </div>
-                  <div className="rounded-full bg-gray-200 px-2 py-1 text-black sm:px-4 sm:py-2">
-                    CSS/HTML
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </section>
   );
 }
--- src/sections/about/AboutCard.jsx
+++ src/sections/about/AboutCard.jsx
@@ -1,3 +1,5 @@
+import SkillPill from "../../ui/SkillPill";
+
 function AboutCard({ index, id, title, description, skills }) {
   return (
     <div
@@ -9,28 +11,20 @@ function AboutCard({ index, id, title, description, skills }) {
     >
       <div className="mb-12 lg:mb-20">
         <div className="flex gap-4 text-3xl font-semibold md:text-5xl">
-          <span className="col-span-2">01.</span>
-          <h3 className="col-span-6 col-start-6">Web Development</h3>
+          <span className="col-span-2">0{index + 1}.</span>
+          <h3 className="col-span-6 col-start-6">{title}</h3>
         </div>
       </div>
 
       <div className="flex flex-col gap-4 md:grid md:grid-cols-12 md:gap-10">
         <div className="col-span-7 col-start-6 space-y-4">
           <p className="text-base font-light text-slate-300 md:max-w-[20rem] lg:max-w-[30rem] lg:text-xl lg:font-normal">
-            Creating modern, responsive, and accessible web applications.
-            Proficient in developing full-stack web applications using modern
-            technologies and best practices.
+            {description}
           </p>
           <div className="flex flex-wrap gap-2 sm:gap-4">
-            <div className="rounded-full bg-gray-200 px-2 py-1 text-black sm:px-4 sm:py-2">
-              React
-            </div>
-            <div className="rounded-full bg-gray-200 px-2 py-1 text-black sm:px-4 sm:py-2">
-              JavaScript
-            </div>
-            <div className="rounded-full bg-gray-200 px-2 py-1 text-black sm:px-4 sm:py-2">
-              CSS/HTML
-            </div>
+            {skills.map((skill) => (
+              <SkillPill skill={skill} key={skill} />
+            ))}
           </div>
         </div>
       </div>
