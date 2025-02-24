acb9113c9127d2ede2f45d22a93ee8ce1bd92b72
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-02-23 20:42:06 -0500

    tweak styling of about card

--- src/sections/about/AboutCard.jsx
+++ src/sections/about/AboutCard.jsx
@@ -9,7 +9,7 @@ function AboutCard({ index, id, title, description, skills }) {
         // marginBottom: `calc(20rem - ${index * 5})`,
       }}
     >
-      <div className="mb-12 lg:mb-20">
+      <div className="mb-12 lg:mb-16">
         <div className="flex gap-4 text-3xl font-semibold md:text-5xl">
           <span className="col-span-2">0{index + 1}.</span>
           <h3 className="col-span-6 col-start-6">{title}</h3>
@@ -18,7 +18,7 @@ function AboutCard({ index, id, title, description, skills }) {
 
       <div className="flex flex-col gap-4 md:grid md:grid-cols-12 md:gap-10">
         <div className="col-span-7 col-start-6 space-y-4">
-          <p className="text-base font-light text-slate-300 md:max-w-[20rem] lg:max-w-[30rem] lg:text-xl lg:font-normal">
+          <p className="mb-10 text-base font-light text-slate-300 md:max-w-[20rem] lg:max-w-[30rem] lg:text-xl lg:font-normal">
             {description}
           </p>
           <div className="flex flex-wrap gap-2 sm:gap-4">
