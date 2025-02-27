faf490a42abdd4d0e248e1a270cfb4699bcc1ee0
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-02-26 20:56:19 -0500

    add url and lenis scroll to button

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import { useLenis } from "lenis/react";
 
-function Button({ children }) {
+function Button({ children, URL }) {
+  const lenis = useLenis();
   return (
-    <Link to={-1}>
+    <Link
+      to={URL}
+      onClick={() => {
+        lenis?.scrollTo(0, { immediate: true });
+      }}
+    >
       <motion.button
         className="rounded-full bg-slate-300 p-3 px-4 font-serif text-base font-semibold text-black transition-all duration-200 hover:bg-slate-400 sm:p-4 sm:px-5 sm:text-lg md:text-xl md:font-medium lg:text-2xl xl:text-3xl xl:font-bold"
         whileHover={{ scale: 1.1 }}
